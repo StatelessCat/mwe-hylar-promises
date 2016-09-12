ba6a699d695ece07fd7aa0922b0771223092a469
Raphaël Cazenave-Lévêque <raphael.cazenave@gmail.com>
2016-09-12 15:56:47 +0200

    No problem using Node.js or Bluebird Promises

--- mwe-hylar-promises.js
+++ mwe-hylar-promises.js
@@ -1,36 +1,40 @@
 const fs = require('fs')
+var Promise = require('bluebird')
 var H = require('hylar')
 
 const mimeType = 'application/ld+json'
 
 const catsOnto = fs.readFileSync('./cats-and-humans.json', 'utf8')
 
-function asyncFunc() {
+const hylarLoadTest = function (jsonld) {
   return new Promise(
     function (resolve, reject) {
-      
-      resolve(result)
-      reject(error)
-    })
-}
-
-const hylarLoadTest = function (jsonld) {
-
-  var Hylar = new H()
-  Hylar.load(jsonld, mimeType)
-    .then(function (response) {
-      return Hylar.getStorage()
-    })
-    .then(function (results) {
-      const ntriples = results.replace(/,/g, '')
-      console.log('#OK#')
-      console.log(ntriples)
-    })
-    .catch(function (err) {
-      console.log('#ERR#', err)
-    })
-  
+      var Hylar = new H()
+      Hylar.load(jsonld, mimeType)
+        .then(function (response) {
+          return Hylar.getStorage()
+        })
+        .then(function (results) {
+          const ntriples = results.replace(/,/g, '')
+          console.log('#OK#')
+          resolve(ntriples)
+        })
+        .then(function () {
+          Hylar.query('construct {?t ?s ?r .}  where {?t ?s ?r .}')
+            .then(function (results) {
+              console.log(results)
+            })
+        })
+        .catch(function (err) {
+          console.log('#ERR#', err)
+          reject(err)
+        })
+    }
+  )
 }
 
 hylarLoadTest(catsOnto)
+  .then(function (res) {
+    console.log(res)
+  })
 
